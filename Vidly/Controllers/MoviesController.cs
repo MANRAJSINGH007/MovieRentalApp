e8b87a4c000c4fceea41688cc78851a610834ba7
MANRAJSINGH007 <manrajs2014@gmail.com>
2020-01-19 20:10:00 +0530

    add customers and movies pages and add details page to view movies a particular customer has rented

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -3,13 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI.WebControls;
 using Vidly.Models;
 using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
+
     public class MoviesController : Controller
     {
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+        {
+            new Movie {Id = 1, Name = "Shrek"},
+            new Movie {Id = 2, Name = "Wall-e"}
+        };
+        }
+        public ActionResult Index()
+        {
+            var movies = GetMovies();
+            return View(movies);
+        }
         // GET: Movies
         public ActionResult Random()
         {
@@ -33,23 +48,6 @@ namespace Vidly.Controllers
             };
             return View(viewModel);
         }
-
-        public ActionResult Edit(int id)
-        {
-            return Content("id= " + id);
-        }
-
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue) pageIndex = 1;
-            if (String.IsNullOrWhiteSpace(sortBy)) sortBy = "Name";
-            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
-        }
-
-        [Route("movies/released/{year}/{month}:regex(\\d{4}):range(1, 12)")]
-        public ActionResult ByReleaseDate(int year, int month)
-        {
-            return Content(year + "/" + month);
-        }
+        
     }
 }
